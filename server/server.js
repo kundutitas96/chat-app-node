75aa60ac06c580af522f27cb565cce29f6845181
Titas Kundu <titaskundu@Titass-MacBook-Air.local>
2018-06-02 13:28:21 +0530

    broadcasting new user joined

--- server/server.js
+++ server/server.js
@@ -15,9 +15,15 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'titasKundu',
-    text: 'Trying to be sober',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => {
@@ -27,6 +33,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
